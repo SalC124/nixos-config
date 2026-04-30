5100a861068f66c3d08bd262174d8aeaf4a2172e
SalC124 <Saltcal124@gmail.com>
2026-04-30 18:35:12 -0400

    added remote builder thing

--- hosts/gamma-compooper/default.nix
+++ hosts/gamma-compooper/default.nix
@@ -13,6 +13,21 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.distributedBuilds = true;
+  nix.buildMachines = [
+    {
+      hostName = "192.168.1.54";
+      system = "x86_64-linux";
+      maxJobs = 2;
+      speedFactor = 2;
+      supportedFeatures = [
+        "nix-command"
+        "flakes"
+        "big-parallel"
+      ];
+    }
+  ];
+
   home-manager.users.${username} = {
     home.stateVersion = "25.11";
   };
